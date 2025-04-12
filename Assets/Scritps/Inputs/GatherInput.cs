7f128feab4a2070cd9dc626058c8b2c5db3c3f6f
BYG80 <gumersantfdz@gmail.com>
2025-04-12 18:23:43 +0200

    Animation Jump and mecanical implemented

--- Assets/Scritps/Inputs/GatherInput.cs
+++ Assets/Scritps/Inputs/GatherInput.cs
@@ -7,9 +7,15 @@ public class GatherInput : MonoBehaviour
 
     private Controls _controls;
 
+    [SerializeField] private bool _isJumping;
+    public bool IsJumping { get => _isJumping; set => _isJumping = value; }
 
-   [SerializeField] private float _valueX;
-   public float ValueX { get => _valueX; }
+
+    [SerializeField] private float _valueX;
+    public float ValueX { get => _valueX; }
+
+
+
 
     // FIRST CALL
     private void Awake()
@@ -23,25 +29,40 @@ public class GatherInput : MonoBehaviour
     {
         _controls.Player.Move.performed += StartMove;
         _controls.Player.Move.canceled += StopMove;
+        _controls.Player.Jump.performed += StartJump;
+        _controls.Player.Jump.canceled += StopJump;
         _controls.Player.Enable();
+
     }
 
     // METHODS FOR CONTROL
 
+    //MOVE
     private void StartMove(InputAction.CallbackContext context)
-    {      
-        _valueX=context.ReadValue<float>();
+    {
+        _valueX = context.ReadValue<float>();
     }
 
     private void StopMove(InputAction.CallbackContext context)
     {
         _valueX = 0f;
     }
+    //ACTION JUMP
+    private void StartJump(InputAction.CallbackContext context)
+    {
+        IsJumping = true;
+    }
+    private void StopJump(InputAction.CallbackContext context)
+    {
+        IsJumping = false;
+    }
     //DISABLE CONTROLS SYSTEM PLAYER
     private void OnDisable()
     {
         _controls.Player.Move.performed -= StartMove;
         _controls.Player.Move.canceled -= StopMove;
+        _controls.Player.Jump.performed -= StartJump;
+        _controls.Player.Jump.canceled -= StopJump;
         _controls.Player.Disable();
     }
 }
--- Assets/Scritps/Inputs/PlayerController.cs
+++ Assets/Scritps/Inputs/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -8,13 +9,23 @@ public class PlayerController : MonoBehaviour
     private GatherInput m_ginput;
     private Transform m_transform;
     private Animator m_animator;
-    
+
 
     //Values
+    [Header("Parameters")]
     [SerializeField] private float _speed;
-    [SerializeField] private int _direction = 1;
-    [SerializeField] private int _idSpeed;
+    [SerializeField] private float _jumpForce;   
+    [SerializeField] private float rayLength;
+    [SerializeField] private int _extraJumps;
+    [SerializeField] private int _counterExtraJumps;
 
+    private int _direction = 1;
+    private int _idSpeed;
+    private int _idGround;
+    [Header("Layers and Raycast distance detection")]
+    [SerializeField] private LayerMask GroundLayer;
+    [SerializeField] private Transform Lfoot, Rfoot;
+    [SerializeField] private bool _isGrounded;
     void Start()
     {
         m_rb = GetComponent<Rigidbody2D>();
@@ -22,26 +33,46 @@ public class PlayerController : MonoBehaviour
         m_transform = GetComponent<Transform>();
         m_animator = GetComponent<Animator>();
         _idSpeed = Animator.StringToHash("_speed");
+        _idGround = Animator.StringToHash("_isGround");
+        Lfoot = GameObject.Find("LFoot").GetComponent<Transform>();
+        Rfoot = GameObject.Find("RFoot").GetComponent<Transform>();
+        _counterExtraJumps = _extraJumps;
+
+
     }
 
     void Update()
     {
         Animations();
-        Flip();
+
     }
     private void FixedUpdate()
     {
-  
-    
+
+
         Move();
+        Jump();
+        CheckGound();
 
     }
+
+
+    // Animations Player
+    private void Animations()
+    {
+        m_animator.SetFloat(_idSpeed, Mathf.Abs(m_rb.linearVelocityX));
+        m_animator.SetBool(_idGround, _isGrounded);
+    }
+    //Movement directional
+
     private void Move()
     {
-        //Movement directional
-        m_rb.linearVelocity = new Vector2(_speed * m_ginput.ValueX, m_rb.linearVelocity.y * Time.deltaTime);
+        Flip();
+        m_rb.linearVelocity = new Vector2(_speed * m_ginput.ValueX, m_rb.linearVelocity.y);
 
     }
+
+    // Flip Player
     private void Flip()
     {
         if (m_ginput.ValueX * _direction < 0)
@@ -52,8 +83,41 @@ public class PlayerController : MonoBehaviour
         }
 
     }
-    private void Animations()
+    //Jump
+    private void Jump()
     {
-        m_animator.SetFloat(_idSpeed, Mathf.Abs(m_rb.linearVelocityX));
+        if (m_ginput.IsJumping)
+        {
+            if (_isGrounded)
+                m_rb.linearVelocity = new Vector2(_speed * m_ginput.ValueX, _jumpForce);
+
+            if (_counterExtraJumps > 0)
+            {
+
+                m_rb.linearVelocity = new Vector2(_speed * m_ginput.ValueX, _jumpForce);
+                _counterExtraJumps--;
+            }
+
+        }
+
+        m_ginput.IsJumping = false;
+
     }
+
+    private void CheckGound()
+    {
+        RaycastHit2D LfootRay = Physics2D.Raycast(Lfoot.position, Vector2.down, rayLength, GroundLayer);
+        RaycastHit2D RfootRay = Physics2D.Raycast(Rfoot.position, Vector2.down, rayLength, GroundLayer);
+        if (LfootRay || RfootRay)
+        {
+            _isGrounded = true;
+            _counterExtraJumps = _extraJumps;
+        }
+        else
+        {
+            _isGrounded = false;
+        }
+    }
+
+
 }

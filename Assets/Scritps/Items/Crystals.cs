3e41d6cef261dc372ecc94507b5123608f0761ac
BYG80 <gumersantfdz@gmail.com>
2025-04-13 23:03:42 +0200

    Cristal animations random

--- Assets/Scritps/Items/Crystals.cs
+++ Assets/Scritps/Items/Crystals.cs
@@ -1,4 +1,7 @@
+using System;
+using Unity;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Crystals : MonoBehaviour
 {
@@ -8,6 +11,7 @@ public class Crystals : MonoBehaviour
     [SerializeField] private Rigidbody2D m_rb;
     [SerializeField] private SpriteRenderer _spriteRenderer;
     [SerializeField] private Animator _animator;
+    private int _idAnim, _idCristalsBlendtree;
 
 
 
@@ -16,25 +20,34 @@ public class Crystals : MonoBehaviour
         m_rb = GetComponent<Rigidbody2D>();
         _animator = GetComponentInChildren<Animator>();
         _spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        _idAnim = Animator.StringToHash("_pickedCristal");
+        _idCristalsBlendtree = Animator.StringToHash("_CristalsAnimation");
     }
 
 
     private void Start()
     {
         _gameManager = GameManager.instance;
+        SetRandomCristals();
     }
-
     private void Update()
     {
 
     }
+    private void SetRandomCristals()
+    {
+        var RandomCristals = Random.Range(0, 7);
+        _animator.SetFloat(_idCristalsBlendtree,RandomCristals);
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            _spriteRenderer.enabled = false;
+       //     _spriteRenderer.enabled = false;
             m_rb.simulated = false;
             _gameManager.AddCristals();
+            _animator.SetTrigger(_idAnim);
         }
 
     }
